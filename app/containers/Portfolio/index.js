9d53f0c0d901967bb1b5c0409b61c8447ea07fc4
Eric Davis <efdavis1@gmail.com>
2017-12-25 15:08:49 -0800

    mock data part 2

--- app/containers/Portfolio/index.js
+++ app/containers/Portfolio/index.js
@@ -11,11 +11,13 @@
 
 import React from 'react';
 import H1 from 'components/H1';
+import H2 from 'components/H2';
+import H3 from 'components/H3';
 import Header from 'components/Header';
 // import H2 from 'components/H2';
 import { Helmet } from 'react-helmet';
-import data from 'mockData.json';
-
+import wallet from 'mockWallet.json';
+import currentPrice from 'currentPrices.json';
 
 export default class Portfolio extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
@@ -29,7 +31,28 @@ export default class Portfolio extends React.PureComponent { // eslint-disable-l
         <H1>
             Portfolio info
         </H1>
-        {console.log(data)};
+        <H3>
+          <H2>Current Holdings</H2> <br></br>
+          LTC {wallet[0].available} <br></br>
+          ETH {wallet[1].available} <br></br>
+          BTC {wallet[2].available} <br></br>
+          Fiat {wallet[3].available} <br></br>
+          BCH {wallet[4].available} <br></br>
+          <br></br>
+          <H2>Current Prices</H2> <br></br>
+          LTC ${currentPrice[0].available} <br></br>
+          ETH ${currentPrice[1].available} <br></br>
+          BTC ${currentPrice[2].available} <br></br>
+          Fiat ${currentPrice[3].available} <br></br>
+          BCH ${currentPrice[4].available} <br></br>
+          <br></br>
+          <H2>Amount in USD</H2> <br></br>
+          LTC {currentPrice[0].available * wallet[0].available} <br></br>
+          ETH {currentPrice[1].available * wallet[1].available} <br></br>
+          BTC {currentPrice[2].available * wallet[2].available} <br></br>
+          Fiat {currentPrice[3].available * wallet[3].available} <br></br>
+          BCH {currentPrice[4].available} <br></br>
+        </H3>
       </div>
     );
   }
