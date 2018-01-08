86067cb9fef3917cafc5a0ecf63dba279e595721
acaciadavis <acaciadavis@gmail.com>
2018-01-07 17:50:46 -0800

    Price endpoint working locally

--- server/market-data.py
+++ server/market-data.py
@@ -16,29 +16,33 @@ class myWebsocketClient(gdax.WebsocketClient):
         if 'price' in msg and 'type' in msg:
             if (not self.found_btc):
                 if (msg["product_id"] == 'BTC-USD'):
-                    self.prices.append(msg["price"])
+                    price = "{:.2f}".format(float(msg["price"]))
+                    self.prices.append(("BTC", price))
                     self.found_btc = True
             if (not self.found_eth):
                 if (msg["product_id"] == 'ETH-USD'):
-                    self.prices.append(msg["price"])
+                    price = "{:.2f}".format(float(msg["price"]))
+                    self.prices.append(("ETH", price))
                     self.found_eth = True
             if (not self.found_bch):
                 if (msg["product_id"] == 'BCH-USD'):
-                    self.prices.append(msg["price"])
+                    price = "{:.2f}".format(float(msg["price"]))
+                    self.prices.append(("BCH", price))
                     self.found_bch = True
             if (not self.found_ltc):
                 if (msg["product_id"] == 'LTC-USD'):
-                    self.prices.append(msg["price"])
+                    price = "{:.2f}".format(float(msg["price"]))
+                    self.prices.append(("LTC", price))
                     self.found_ltc = True
     def on_close(self):
         print self.prices
-        print("-- Goodbye! --")
+        print "close"
 
 ws_client = myWebsocketClient()
 ws_client.start()
 
-while ((not ws_client.found_eth) and (not ws_client.found_btc) and (not ws_client.found_ltc) and (not ws_client.found_eth)):
+while ((not ws_client.found_eth) and (not ws_client.found_btc) and (not ws_client.found_ltc) and (not ws_client.found_bch)):
     time.sleep(1)
     #keep going
-print("found em all")
+
 ws_client.close()
