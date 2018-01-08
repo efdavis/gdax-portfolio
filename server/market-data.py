34a6252c7a991358213f14505b418939b8856c62
acaciadavis <acaciadavis@gmail.com>
2018-01-07 16:44:01 -0800

    price ticker

--- server/market-data.py
+++ server/market-data.py
@@ -1,41 +1,44 @@
 import gdax, time
 
-foundEth = False;
-foundBtc = False;
-foundBch = False;
-foundLtc = False;
-
 # Get price ticker information from feed
 class myWebsocketClient(gdax.WebsocketClient):
-    
 
     def on_open(self):
         self.url = "wss://ws-feed.gdax.com/"
         self.products = ["ETH-USD", "BTC-USD", "LTC-USD", "BCH-USD"]
-        self.message_count = 0
-        print("BTC-USD")
+        self.found_eth = False
+        self.found_btc = False
+        self.found_ltc = False
+        self.found_bch = False
+        self.prices = []
+
     def on_message(self, msg):
-        self.message_count += 1
         if 'price' in msg and 'type' in msg:
-           # print("Currency type", msg["product_id"],  "Price: {:.3f}".format(float(msg["price"])))
-            if (msg["product_id"] == 'BTC-USD'):
-                foundBtc = True;
-            if (msg["product_id"] == 'ETH-USD'):
-                foundEth = True;
-            if (msg["product_id"] == 'BCH-USD'):
-                foundBch == True;
-            if (msg["product_id"] == 'LTC-USD'):
-                foundLtc == True;
+            if (not self.found_btc):
+                if (msg["product_id"] == 'BTC-USD'):
+                    self.prices.append(msg["price"])
+                    self.found_btc = True
+            if (not self.found_eth):
+                if (msg["product_id"] == 'ETH-USD'):
+                    self.prices.append(msg["price"])
+                    self.found_eth = True
+            if (not self.found_bch):
+                if (msg["product_id"] == 'BCH-USD'):
+                    self.prices.append(msg["price"])
+                    self.found_bch = True
+            if (not self.found_ltc):
+                if (msg["product_id"] == 'LTC-USD'):
+                    self.prices.append(msg["price"])
+                    self.found_ltc = True
     def on_close(self):
+        print self.prices
         print("-- Goodbye! --")
 
-wsClient = myWebsocketClient()
-wsClient.start()
-print(wsClient.url, wsClient.products)
+ws_client = myWebsocketClient()
+ws_client.start()
 
-
-while ((not foundEth) or (not foundBtc) or (not foundLtc) or (not foundBch)):
-    #do nothing
-    #print ("\nmessage_count =", "{} \n".format(wsClient.message_count))
+while ((not ws_client.found_eth) and (not ws_client.found_btc) and (not ws_client.found_ltc) and (not ws_client.found_eth)):
     time.sleep(1)
-wsClient.close()
+    #keep going
+print("found em all")
+ws_client.close()
